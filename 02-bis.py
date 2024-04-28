f587f865a2766dcba2fb0c130f3cc0d6b0a10ca9
Amandine Souilleux <amandine.souilleux@dataiku.com>
2024-04-28 20:20:56 +0200

    exo 2 finished

--- 02-bis.py
+++ 02-bis.py
@@ -1,6 +1,6 @@
 import copy
 
-INPUT_FILE = '02-test.txt'
+INPUT_FILE = '02-input.txt'
 
 class Time:
     def __init__(self, value, path=[], depth=1):
@@ -17,13 +17,15 @@ def compute():
     global_max_path = []
     with open(INPUT_FILE, 'r') as f:
         input = f.readlines()
-        for value_s in input:
-            value = get_clean_value(value_s)
-            print(f"Value: {value}")
+        for i in range(len(input)):
+            value = get_clean_value(input[i])
+            print(i)
+            #print(f"Value: {value}")
             max_depth = 1
             max_path = [value]
+            times_to_remove = []
             for time in computed_list:
-                print(f"Comparing to time: {time.value} with depth {time.depth} and path {time.path}")
+                #print(f"Comparing to time: {time.value} with depth {time.depth} and path {time.path}")
                 if value < time.value and max_depth <= time.depth:
                     max_depth = time.depth+1
                     max_path = time.path.copy()
@@ -32,7 +34,7 @@ def compute():
             if max_depth > global_max_depth:
                 global_max_depth = max_depth
                 global_max_path = max_path
-                print(f"Max depth: {global_max_depth}, max path: {global_max_path}")
+                #print(f"Max depth: {global_max_depth}, max path: {global_max_path}")
     return global_max_path
 
 def main():
